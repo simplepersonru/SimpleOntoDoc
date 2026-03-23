ba93faebd895432ab612db431f12b32f467a5594
ArtemBabaev <trallkol2@bk.ru>
2026-03-24 03:11:57 +0500

    улучшение примера

--- Example/example.bash
+++ Example/example.bash
@@ -3,8 +3,8 @@ cd "$SCRIPT_DIR" || exit 1
 
 # SIMPLEDOC_OUT_DOCKER_CONTAINER_TAG заменить на какой-нибудь свой
 
-SIMPLEDOC_INPUT_PATH=$SCRIPT_DIR/test_ontology_classes.json \
-SIMPLEDOC_TITLE="Food ontology documentation" \
-SIMPLEDOC_DESCRIPTION="Food / Restaurant / Food Supply Chain ecosystem ontological scheme that includes some diversity" \
+SIMPLEDOC_INPUT_PATH=$SCRIPT_DIR/cgmes_example.json \
+SIMPLEDOC_TITLE="Common Grid Model Exchange Standard (CGMES)" \
+SIMPLEDOC_DESCRIPTION="CGMES (Common Grid Model Exchange Standard) is a data exchange standard for power systems developed by ENTSO-E. It enables transmission system operators to share consistent grid models for coordinated planning, analysis, and operation of interconnected electricity networks." \
 SIMPLEDOC_OUT_DOCKER_CONTAINER_TAG="gitea.simpleperson.ru/pub/test-ontology:latest" \
 /usr/bin/bash $SCRIPT_DIR/../publish.bash
